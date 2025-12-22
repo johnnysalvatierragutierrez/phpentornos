1027140e776fae3b5af957696a12a258111512d9
alvaro <roilt532@gmail.com>
2025-12-22 23:58:21 +0100

    Corrijo función: añado control de sesión por tiempo

--- cap6/sesiones_json.php
+++ cap6/sesiones_json.php
@@ -1,7 +1,15 @@
 <?php
 function comprobar_sesion(){
 	session_start();
-	if(!isset($_SESSION['usuario'])){	
+	$timeout_seconds = 1800;
+	if(!isset($_SESSION['usuario'])){
 		return false;
-	}else return true;		
+	}
+	if(isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout_seconds)){
+		session_unset();
+		session_destroy();
+		return false;
+	}
+	$_SESSION['LAST_ACTIVITY'] = time();
+	return true;
 }
