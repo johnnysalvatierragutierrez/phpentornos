5caffc5139dc42c652a9c5fe5c1e53713ab5dccb
johnnysalvatierragutierrez <simplementebecoinf@gmail.com>
2025-12-23 14:19:41 +0100

    Correcion de evalucion de sessiones

--- cap6/sesiones_json.php
+++ cap6/sesiones_json.php
@@ -1,7 +1,25 @@
-<?php
-function comprobar_sesion(){
-	session_start();
-	if(!isset($_SESSION['usuario'])){	
-		return false;
-	}else return true;		
+<?php 
+function comprobar_sesion(int $timeout = 900): bool { // 900s = 15 min 
+    session_start(); 
+  
+ 
+    // Si no hay usuario, no hay sesión válida 
+    if (!isset($_SESSION['usuario'])) { 
+        return false; 
+    } 
+ 
+    $ahora = time(); 
+ 
+    // Si existe marca de actividad y ha expirado -> cerrar sesión 
+ 
+    if (isset($_SESSION['ultima_actividad']) && ($ahora - 
+$_SESSION['ultima_actividad']) > $timeout) { 
+        
+        return false; 
+    } 
+ 
+    // Actualiza actividad 
+    $_SESSION['ultima_actividad'] = $ahora; 
+ 
+    return true; 
 }
